d28bb92f2a500e4bfa279c582e1a4d7736fc3425
Ricardo <rick_ferry1@hotmail.com>
2024-08-28 12:09:50 -0300

    Add model and repository

---
+++ src/main/java/com/bnex/storeapi/model/Produto.java
@@ -0,0 +1,29 @@
+package com.bnex.storeapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal valor;
+}
---
+++ src/main/java/com/bnex/storeapi/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.bnex.storeapi.repository;
+
+import com.bnex.storeapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
